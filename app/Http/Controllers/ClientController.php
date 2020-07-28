ce0cb021a1a9c95a20bb145d5f1f617c9671e824
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2020-07-27 22:16:25 -0500

    Nuevos métodos para cambiar el estado 'enabled' a un usuario

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $clients = User::orderBy('id', 'Desc')->paginate(5);
@@ -19,74 +15,27 @@ class ClientController extends Controller
         return view('client.index', compact('clients'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(int $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(int $id)
     {
-        $user = User::findOrFail($id);
-        return view('client.edit', ['client' => $user]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, int $id)
+
+    public function update(int $id)
     {
-
-        $user = User::findOrFail($id);
-        if ($user->enabled == true) {
-            $user->enabled = false;
-            $user->save();
-            return redirect()->route('client.disable');
-        } else {
-            $user->enabled = true;
-            $user->save();
-            return redirect()->route('client.index');
-        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(int $id)
     {
         //
@@ -97,5 +46,23 @@ class ClientController extends Controller
         $clients = User::orderBy('id', 'Desc')->paginate(5);
         return view('client.disable', compact('clients'))
             ->with('status_success', 'Accesibilidad cambiada correctamente');
+    }
+    public function changeState(int $id)
+    {
+        $user = User::findOrFail($id);
+        return view('client.confirmChangeStatus', ['client' => $user]);
+    }
+    public function updateState(int $id)
+    {
+        $user = User::findOrFail($id);
+        if ($user->enabled == true) {
+            $user->enabled = false;
+            $user->save();
+            return redirect()->route('client.disable');
+        } else {
+            $user->enabled = true;
+            $user->save();
+            return redirect()->route('client.index');
+        }
     }
 }
