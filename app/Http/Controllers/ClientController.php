b3725891abf6ed8477dde4555ddefa668971b796
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2020-08-22 20:15:55 -0500

    Estilo: Actualizacion de vistas
    Se elimino el card header por un titulo h1, tambien la lista de los clientes tiene menos informacion y el boton ver funciona, y el boton editar; al editar un usuario tengo la aocion de habilitar o deshabiliar.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use App\Actions\Clients\UpdateClientState;
+
+use App\RolesAndPermissions\Models\Role;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
@@ -15,27 +17,37 @@ class ClientController extends Controller
 
     public function index(): View
     {
-        $clients = User::orderBy('id', 'Desc')->paginate(5);
-        return view('client.index', compact('clients'));
-    }
-
-    public function store(Request $request)
-    {
-        //
+        return view('client.index', ['clients' => User::with('roles')->orderBy('id', 'Desc')->paginate(5)]);
     }
 
     public function show(int $id)
     {
-        //
+
+        $client = User::findorFail($id);
+        //$this->authorize('view', $client);
+        return view('client.show', ['client' => $client]);
     }
 
     public function edit(int $id)
     {
+        $client = User::findorFail($id);
+        if ($client['enabled'] == true) {
+            $enabledButton = 'Deshabilitar usuario';
+        } else {
+            $enabledButton = 'Habilitar usuario';
+        }
+
+        $roles = Role::orderBy('name')->get();
+        return view('client.edit', ['client' => $client, 'roles' => $roles, 'enabledButton' => $enabledButton]);
     }
 
 
-    public function update(int $id)
+    public function update(int $id, Request $request)
     {
+        $client = User::findorFail($id);
+        $client->roles()->sync($request->get('roles'));
+        return redirect()->route('client.index')
+            ->with('status_success', 'Usuario actualizado correctamente');
     }
 
 
@@ -54,8 +66,8 @@ class ClientController extends Controller
     public function changeState(int $id): View
     {
         $this->authorize('haveAccess', 'client.edit');
-        $user = User::findOrFail($id);
-        return view('client.confirmChangeStatus', ['client' => $user]);
+        $client = User::findOrFail($id);
+        return view('client.confirmChangeStatus', ['client' => $client]);
     }
     public function updateState(int $id): RedirectResponse
     {
--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -1,1 +1,27 @@
-no hay nadita jeje
+@extends('layouts.app')
+@section('title','Cliente | ' . $client->name )
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-offset-3">
+            <img class="img-responsive menu-thumbnails" style="width:331.7px;height:429px;" src="/images/logos/logoC.png"/>
+        </div>
+        <div class="col">
+            <h1 class="text-center" style="padding-bottom: 10px">Informacion del usuario</h1>
+            <div class="card border-success">
+                <ul class="list-group list-group-flush align-items-center">
+                    <li class="list-group-item"><h3>Nombre: {{$client->name}}</h3></li>
+                    <li class="list-group-item"><h3>Direccion: {{$client->address}}</h3></li>
+                    <li class="list-group-item"><h3>Celular: {{$client->phone}}</h3></li>
+                    <li class="list-group-item"><h3>Correo: {{$client->email}}</h3></li>
+                    <li class="list-group-item"><h3>Rol:
+                        @isset($client->roles[0]->name)
+                        {{$client->roles[0]->name}}
+                    @endisset</h3></li>
+                </ul>
+                <a class="btn btn-outline-success float-right" href="{{route('client.edit', $client)}}">Editar</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
